d54886368af5f10e253ff47df8406c09e96c8da0
Chandan Dubey <dubeychandan411@gmail.com>
2025-10-22 19:08:29 +0530

    Fix: corrected logo image path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import logo from "../assets/images/logo.png";
+import logo from "../assets/Images/logo.png";
 import { FaBars } from "react-icons/fa";
 import { FaXmark } from "react-icons/fa6";
 
